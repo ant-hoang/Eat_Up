cde96bd42e134644d78a10e412124f01c41527a8
ruokmain <anthony.hoang1991@gmail.com>
2024-04-12 18:42:32 -0700

    Add User input validation on user signup backend endpoint

---
+++ backend/utils/validators/users.js
@@ -0,0 +1,32 @@
+const { check } = require('express-validator')
+const { handleValidationErrors } = require('../validation')
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  check('firstName')
+    .exists({ checkFalsey: true })
+    .notEmpty()
+    .withMessage('First Name is required'),
+  check('lastName')
+    .exists({ checkFalsey: true })
+    .notEmpty()
+    .withMessage('Last Name is required'),
+  handleValidationErrors
+]
+
+module.exports = { validateSignup }
