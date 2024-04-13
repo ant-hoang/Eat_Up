e53e4e3b7347dac3bba6f765fad4c6ff30e26bc5
ruokmain <anthony.hoang1991@gmail.com>
2024-04-12 19:11:39 -0700

    changed migrations file to add options

--- backend/db/migrations/20240411085656-create-user.js
+++ backend/db/migrations/20240411085656-create-user.js
@@ -47,10 +47,9 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = "Users"
-    await queryInterface.dropTable(options);
+    await queryInterface.dropTable('Users');
   }
 };
