0238051cd97d172a02653b538b7066f0b0b6a295
ruokmain <anthony.hoang1991@gmail.com>
2024-04-17 20:01:48 -0700

    added Recipes and Ingredients models/migrations to the backend db

---
+++ backend/db/models/recipe.js
@@ -0,0 +1,78 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Recipe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Recipe.belongsTo(models.User, { foreignKey: 'userId', as: 'User' })
+      Recipe.hasMany(models.Ingredient, { foreignKey: 'recipeId', onDelete: 'CASCADE', hooks: true })
+    }
+  }
+  Recipe.init({
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE'
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
+    },
+    origin: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
+    },
+    directions: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // validate: {
+      //   notEmpty: true
+      // }
+    },
+    // Additional Info to consider adding
+    // prepTime: {
+    //   type: DataTypes.INTEGER
+    // },
+    // cookTime: {
+    //   type: DataTypes.INTEGER
+    // },
+    // totalTime: {
+    //   type: DataTypes.INTEGER
+    // },
+    // servings: {
+    //   type: DataTypes.INTEGER
+    // },
+    video: {
+      type: DataTypes.STRING,
+      defaultValue: ''
+    },
+    images: {
+      type: DataTypes.STRING,
+      defaultValue: ''
+    }
+  }, {
+    sequelize,
+    modelName: 'Recipe',
+  });
+  return Recipe;
+};
