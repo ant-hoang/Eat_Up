8a6911ff3f66d48d53b88fe8fadfdd3d613f245f
ruokmain <anthony.hoang1991@gmail.com>
2024-04-18 14:12:29 -0700

    optimized the get routes for recipe api

--- backend/routes/api/recipes.js
+++ backend/routes/api/recipes.js
@@ -10,69 +10,39 @@ const router = express.Router()
 
 router.get('/current', requireAuth, async (req, res) => {
   const user_id = req.user.id
-  const recipes = await Recipe.findAll({ where: { userId: user_id }})
+  const recipes = await Recipe.findAll({ 
+    where: { userId: user_id },
+    include: { model: Like }
+  })
 
   let recipesJSON = []
   for (const recipe of recipes) {
-    const { count } = await Like.findAndCountAll({ where: { recipeId: recipe.id}})
     let setRecipe = recipe.toJSON()
-    setRecipe.numLikes = count
+    setRecipe.numLikes = setRecipe.Likes.length
+    delete setRecipe.Likes
     recipesJSON.push(setRecipe)
   }
-  
+
   res.json({ "Recipes": recipesJSON })
 })
 
 router.get('/:recipeId', async (req, res, next) => {
-  // const { recipeId } = req.params
-  
-  // try {
-  //   const recipe = await Recipe.findOne({ 
-  //     where: { id: +recipeId},
-  //     // attributes: {
-  //     //   include: [[sequelize.fn('COUNT', sequelize.col('Likes.recipeId')), 'numLikes']]
-  //     // },
-  //     include: [
-  //       { model: User, attributes: ['id', 'username'] },
-  //       { model: Ingredient, attributes: ['id', 'name', 'quantity', 'metric'] },
-  //       // { model: Like, attributes: [], where: { recipeId: +recipeId}},
-  //     ]
-  //   })
-  //   console.log("RECIPE!!!!", recipe)
-  //   // const { count } = await Like.findAndCountAll({ where: { recipeId: recipe.id}})
-  //   // const ingredients = await Ingredient.findAll({ where: { recipeId: recipe.id}})
-  //   // console.log("INGREDIENTS: ", ingredients)
-
-  //   res.json({ recipe })
-
-  // } catch (err) {
-  //   err.message = 'Recipe couldn\'t be found'
-  //   err.status = 404
-  //   return next(err)
-  // }
-
-
-
   const { recipeId } = req.params;
 
   try {
     const recipe = await Recipe.findOne({
       where: { id: +recipeId },
-      attributes: {
-        include: [[sequelize.fn('COUNT', sequelize.literal('DISTINCT Likes.userId')), 'numLikes']]
-      },
       include: [
         { model: User, attributes: ['id', 'username'] },
         { model: Ingredient, attributes: ['id', 'name', 'quantity', 'metric'] },
-        {
-          model: Like,
-          where: { recipeId: +recipeId },
-          attributes: []
-        }
       ]
     });
 
-    res.json({ recipe });
+    const count = await Like.count({ where: { recipeId: recipe.id } })
+    const recipeJSON = recipe.toJSON()
+    recipeJSON.numLikes = count
+
+    res.json({ recipeJSON });
   } catch (err) {
     err.message = 'Recipe couldn\'t be found';
     err.status = 404;
@@ -81,18 +51,17 @@ router.get('/:recipeId', async (req, res, next) => {
 })
 
 router.get('/', async (req, res) => {
-  const recipes = await Recipe.findAll()
+  const recipes = await Recipe.findAll({ include: {model: Like} })
+
   let recipesJSON = []
   for (const recipe of recipes) {
-    const { count } = await Like.findAndCountAll({ where: { recipeId: recipe.id}})
     let setRecipe = recipe.toJSON()
-    setRecipe.numLikes = count
+    setRecipe.numLikes = setRecipe.Likes.length
+    delete setRecipe.Likes
     recipesJSON.push(setRecipe)
   }
 
   res.json({ "Recipes": recipesJSON })
 })
-
-
 
 module.exports = router
